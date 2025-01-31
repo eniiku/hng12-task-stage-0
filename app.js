bb9d096f2a32eb9f2f8ed7ad8cbd84c7db082d24
David Enikuomehin <enikuauth@gmail.com>
2025-01-31 21:49:51 +0100

    feat: create api route with required response

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
+
+app.use(cors())
+
+app.get("api/v1/details", (req, res) => {
+  res.status(200).json({
+    email: "enikuauth@gmail.com",
+    current_datetime: new Date().toISOString(),
+    github_url: "https://github.com/eniiku/hng12-task-stage-0.git",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
